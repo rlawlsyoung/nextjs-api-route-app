8c17f5e1c5f67048f09428d6d65c7cd824a1437c
김진영 <jinyoung01099@gmail.com>
2023-04-25 20:57:49 +0900

    [Add] registration api 유효성 검사 추가

--- pages/api/registration/index.ts
+++ pages/api/registration/index.ts
@@ -3,6 +3,11 @@ import { NextApiRequest, NextApiResponse } from "next";
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
     const email = req.body.email;
+
+    if (!email || !email.includes("@")) {
+      res.status(422).json({ message: "Invalid email address" });
+      return;
+    }
     res.status(201).json({ email: email });
   }
 };
