0bd471701465ad2bb7b85ff5bb865de5385945c7
김진영 <jinyoung01099@gmail.com>
2023-04-25 21:13:12 +0900

    [Add] comments api 유효성 검사 추가

--- pages/api/comments/[eventId].ts
+++ pages/api/comments/[eventId].ts
@@ -1,16 +1,28 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
+  const eventId = req.query.eventId;
+
   if (req.method === "GET") {
     res.status(200).json({
       data: [
-        { email: "1@test.com", name: "kim", text: "hi" },
-        { email: "2@test.com", name: "park", text: "bye" },
+        { id: "c1", email: "1@test.com", name: "kim", text: "hi" },
+        { id: "c2", email: "2@test.com", name: "park", text: "bye" },
       ],
     });
   } else if (req.method === "POST") {
+    const { email, name, text } = req.body;
+    if (
+      !email.includes("@") ||
+      !name ||
+      name.trim() === "" ||
+      text.trim() === ""
+    ) {
+      res.status(422).json({ message: "Invalid Input" });
+      return;
+    }
     res.status(201).json({
-      data: { email: req.body.email, name: req.body.name, text: req.body.text },
+      data: { email: email, name: name, text: text },
     });
   }
 };
