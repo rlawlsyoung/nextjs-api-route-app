2e4111f04e138617a80731ebb8ba2a4ce17040b6
김진영 <jinyoung01099@gmail.com>
2023-04-28 20:59:08 +0900

    [Add] MongoDB와 연동하여 newsletter 구독 기능 추가

--- pages/api/registration/index.ts
+++ pages/api/registration/index.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { MongoClient } from "mongodb";
 
-const handler = (req: NextApiRequest, res: NextApiResponse) => {
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
     const email = req.body.email;
 
@@ -8,6 +9,17 @@ const handler = (req: NextApiRequest, res: NextApiResponse) => {
       res.status(422).json({ message: "Invalid email address" });
       return;
     }
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://rlawlsyoung:1q2w3e4r@udemycluster.o9wuh6s.mongodb.net/?retryWrites=true&w=majority"
+    );
+
+    const db = client.db("newsletter");
+
+    await db.collection("emails").insertOne({ email: email });
+
+    client.close();
+
     res.status(201).json({ email: email });
   }
 };
