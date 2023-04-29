a71b513b2086da51bde7fcd48accf0c759fd30c5
김진영 <jinyoung01099@gmail.com>
2023-04-29 18:29:34 +0900

    [Add] registration api 에러 핸들링 추가

--- pages/api/registration/index.ts
+++ pages/api/registration/index.ts
@@ -1,6 +1,23 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { MongoClient } from "mongodb";
 
+const connectDatabase = async () => {
+  const client = await MongoClient.connect(
+    "mongodb+srv://rlawlsyoung:1q2w3e4r@udemycluster.o9wuh6s.mongodb.net/?retryWrites=true&w=majority"
+  );
+
+  return client;
+};
+
+const insertDocument = async (
+  client: MongoClient,
+  document: { email: string }
+) => {
+  const db = client.db("events");
+
+  await db.collection("newsletter").insertOne(document);
+};
+
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
     const email = req.body.email;
@@ -10,15 +27,22 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       return;
     }
 
-    const client = await MongoClient.connect(
-      "mongodb+srv://rlawlsyoung:1q2w3e4r@udemycluster.o9wuh6s.mongodb.net/?retryWrites=true&w=majority"
-    );
+    let client;
 
-    const db = client.db("events");
+    try {
+      client = await connectDatabase();
+    } catch (err) {
+      res.status(500).json({ message: "Connecting to the database failed." });
+      return;
+    }
 
-    await db.collection("newsletter").insertOne({ email: email });
-
-    client.close();
+    try {
+      await insertDocument(client, { email: email });
+      client.close();
+    } catch (err) {
+      res.status(500).json({ message: "Inserting database failed." });
+      return;
+    }
 
     res.status(201).json({ email: email });
   }
